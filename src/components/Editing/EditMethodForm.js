f733e1362bf43bcc4d79d1f41eaef95cfe80504d
dietBlue <jeff.charters@gmail.com>
2020-07-19 20:44:38 -0400

    edit method loads method from app

--- src/components/Editing/EditMethodForm.js
+++ src/components/Editing/EditMethodForm.js
@@ -1,51 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams, useHistory } from 'react-router-dom'
+import axios from 'axios'
 import SingleInput from './SingleInput'
 import UnitInput from './UnitInput'
 import MethodObjectForm from './MethodObjectForm'
 import ElementHeader from './ElementHeader'
 
-const aMethod = {
-  name: "CHEM-162",
-  description: "Metals in Blood and Serum",
-  elements: ["Mn", "Fe", "Co", "Cu", "Zn", "Se", "Mo", "Pb"],
-  units: ["ppb", "ppm", "ppb", "ppm", "ppm", "ppm", "ppb", "ppm"],
-  checkStds: [
-    {
-      name: '5/50 ppb',
-      expectedValues: [5, 50, 5, 50, 50, 5, 5, 5]
-    }
-  ],
-  blanks: [
-    {
-      name: 'Serum Blank',
-      type: 'serum',
-      LOQs: [0.9, 0.013, 0.3, 0.0008, 0.0011, 0.007, 1, null]
-    },
-    {
-      name: 'Blood Blank',
-      type: 'blood',
-      LOQs: [null, null, null, null, null, 0.029, null, 0.001]
-    }
-  ],
-  checkStdTolerance: 0.1,
-  duplicateTolerance: 15,
-  calStandards: [0.05, 0.1, 0.25, 0.5, 1, 5, 10, 50, 250],
-  sigFigs: 3,
-  referenceMaterials: [{
-    name: 'QM-S Q1807',
-    rangesLow: [2.6, 0.7, 3.7, 0.9, 0.9, 0.1, 1.0, null],
-    rangesHigh: [3.1, 1.0, 4.2, 1.2, 1.2, 0.15, 1.7, null]
-  },
-  {
-    name: 'QM-B Q1720',
-    rangesLow: [null, null, null, null, null, 0.15, null, 0.10],
-    rangesHigh: [null, null, null, null, null, 0.20, null, 0.14]
-  }]
-}
-
 const EditMethodForm = () => {
 
-  const [method, setMethod] = useState(aMethod)
+  const [method, setMethod] = useState()
+  const methodName = useParams().name
+  const history = useHistory()
+
+  const baseUrl = 'http://localhost:3001'
+
+  useEffect(() => {
+    axios.get(`${baseUrl}/api/methods`)
+      .then(response => {
+        setMethod(response.data.find(m => m.name === methodName))
+      })
+  }, [methodName])
+
+  if (!method) {
+    return 'Loading...'
+  }
 
   const methodChangeHandler = (event) => {
     setMethod({
@@ -412,14 +390,24 @@ const EditMethodForm = () => {
         <hr />
 
         <div style={{ textAlign: 'right' }}>
-          <button
-            type="submit"
-            onClick={saveChanges}
-            style={{ margin: '15px', padding: '3px' }}
-          >
-            Save Changes
+          <div style={{ display: 'inline', textAlign: 'right' }}>
+            <button
+              type="submit"
+              onClick={saveChanges}
+              style={{ margin: '15px', padding: '3px' }}
+            >
+              Save Changes
           </button>
-
+          </div>
+          <div style={{ display: 'inline', textAlign: 'left' }}>
+            <button
+              type="submit"
+              onClick={() => history.push('/')}
+              style={{ margin: '15px', padding: '3px' }}
+            >
+              Cancel and Leave
+          </button>
+          </div>
         </div>
 
       </form>
